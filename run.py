b9aeb4e7ae61a7dff9463c8ef715fa6267f4602b
Restyled.io <commits@restyled.io>
2021-08-28 20:30:11 +0000

    Restyled by autopep8

--- run.py
+++ run.py
@@ -1,76 +1,79 @@
 #!/usr/bin/env python3
-import os, sys, subprocess, shutil
+import os
+import sys
+import subprocess
+import shutil
 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument(
-  "--firefox",
-  help="Override the default binary",
-  type=str,
+    "--firefox",
+    help="Override the default binary",
+    type=str,
 )
 
 args = parser.parse_args()
 if args.firefox:
-  firefox = args.firefox
+    firefox = args.firefox
 elif sys.platform == "darwin":
-  firefox = "/Applications/Firefox.app/Contents/MacOS/firefox"
+    firefox = "/Applications/Firefox.app/Contents/MacOS/firefox"
 elif sys.platform[0:5] == "linux":
-  firefox = "/usr/bin/firefox"
+    firefox = "/usr/bin/firefox"
 else:
-  print("Unsupported operating system")
-  sys.exit(1)
-
-if not os.path.isdir("./profile"):
-  os.mkdir("./profile");
-
-if not os.path.isdir("./xulapp/browser"):
-  os.mkdir("./xulapp/browser")
-  if sys.platform == "darwin":
-    jarfile = "/Applications/Firefox.app/Contents/Resources/browser/omni.ja"
-  elif sys.platform[0:5] == "linux":
-    jarfile = "/usr/lib64/firefox/browser/omni.ja"
-  else:
-    print("Unsupported operating system (browser jar)")
+    print("Unsupported operating system")
     sys.exit(1)
 
-  # extract the jar file
-  subprocess.call(
-    [
-      '/usr/bin/unzip',
-      jarfile,
+if not os.path.isdir("./profile"):
+    os.mkdir("./profile")
 
-      '-d',
-      './xulapp/browser/',
-    ]
-  )
-
-  # Rewrite xulapp/browser/chrome/chrome.manifest
-  manifestFile = open("xulapp/browser/chrome/chrome.manifest", "r")
-  manifest = ""
-  for line in manifestFile:
-    if "branding" in line:
-      pass
-    elif "override" in line:
-      pass
+if not os.path.isdir("./xulapp/browser"):
+    os.mkdir("./xulapp/browser")
+    if sys.platform == "darwin":
+        jarfile = "/Applications/Firefox.app/Contents/Resources/browser/omni.ja"
+    elif sys.platform[0:5] == "linux":
+        jarfile = "/usr/lib64/firefox/browser/omni.ja"
     else:
-      manifest += line
-  manifestFile.close()
+        print("Unsupported operating system (browser jar)")
+        sys.exit(1)
 
-  manifestFile = open("xulapp/browser/chrome/chrome.manifest", "w")
-  manifestFile.write(manifest)
-  manifestFile.close()
+    # extract the jar file
+    subprocess.call(
+        [
+            '/usr/bin/unzip',
+            jarfile,
+
+            '-d',
+            './xulapp/browser/',
+        ]
+    )
+
+    # Rewrite xulapp/browser/chrome/chrome.manifest
+    manifestFile = open("xulapp/browser/chrome/chrome.manifest", "r")
+    manifest = ""
+    for line in manifestFile:
+        if "branding" in line:
+            pass
+        elif "override" in line:
+            pass
+        else:
+            manifest += line
+    manifestFile.close()
+
+    manifestFile = open("xulapp/browser/chrome/chrome.manifest", "w")
+    manifestFile.write(manifest)
+    manifestFile.close()
 
 subprocess.call(
-  [
-    firefox,
+    [
+        firefox,
 
-    "--app",
-    "./xulapp/application.ini",
+        "--app",
+        "./xulapp/application.ini",
 
-    "-profile",
-    "./profile",
+        "-profile",
+        "./profile",
 
-    "--no-remote",
-  ]
+        "--no-remote",
+    ]
 )
 print("\n")
